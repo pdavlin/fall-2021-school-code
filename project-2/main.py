636599f2eacd9293daa4d046ad7ef096c920b0b6
Patrick Davlin <pdavasaurusrex@gmail.com>
2021-11-18 22:09:12 -0600

    gini index idk

--- project-2/main.py
+++ project-2/main.py
@@ -9,26 +9,42 @@ with open('./dataset-for-Project2.csv', 'r') as dataset_file:
 # Parse the dataset into a list of vectors
 vectors = []
 labels = []
+labels_unique = []
 for line in lines:
     line = line.strip()
     line_vals = line.split(',')
     vectors.append(line_vals[1:-1])
-    labels.append(line_vals[-1])
-# print(labels)
+    label = line_vals[-1]
+    labels.append(label)
+    if label not in labels_unique:
+        labels_unique.append(label)
+# print(labels_unique)
 
 word_names = vectors[0]
 
 # Remove the first line of IDs from the dataset
 vectors = vectors[1:]
 labels = labels[1:]
+labels_unique = labels_unique[1:]
+labels_dict = {}
+for i in range(len(labels_unique)):
+    labels_dict[labels_unique[i]] = i
+# print(labels_dict)
 
-def test_classifier(newsgroup_train, newsgroup_test, classifier, labels):
+labels_as_ints = []
+for i in range(len(labels)):
+    labels_as_ints.append(labels_dict[labels[i]])
+# labels_unique_int = [i for i in range(len(labels_unique))]
+# print(labels_unique_int)
 
-    labels_train = labels[:11175]
-    labels_test = labels[11175:]
-    classifier.fit(newsgroup_train, labels_train)
-    predictions = classifier.predict(newsgroup_test)
-    accuracy = classifier.score(newsgroup_test, labels_test)
+    
+
+def test_classifier(data_train, data_test, classifier, labels_train, labels_test):
+
+
+    classifier.fit(data_train, labels_train)
+    predictions = classifier.predict(data_test)
+    accuracy = classifier.score(data_test, labels_test)
 
 
     cfm = confusion_matrix(labels_test, predictions)
@@ -36,33 +52,41 @@ def test_classifier(newsgroup_train, newsgroup_test, classifier, labels):
     false_negatives = cfm[1][0]
     print('accuracy: {}. {} false positives, {} false negatives'.format(accuracy, false_positives, false_negatives))
 
+def three_classifier_tests(data_train, data_test, labels_train, labels_test):
+    classifier = RandomForestClassifier()
+    test_classifier(data_train, data_test, classifier, labels_train, labels_test)
+    
+    classifier = RandomForestClassifier(n_estimators=100, max_depth=100, random_state=95)
+    test_classifier(data_train, data_test, classifier, labels_train, labels_test)
+
+
 def calc_gini_index(data, labels):
     _, features = data.shape
-    gini = np.ones(features) * 0.5
+    gini = np.ones(features)
     # print(features)
     for i in range(features):
         v = np.unique(data[:,i])
         for j in range(len(v)):
-            left_docs = labels[data[:,i] <= v[j]]
-            right_docs = labels[data[:, i] > v[j]]
+            left_y = labels[data[:,i] <= v[j]]
+            right_y = labels[data[:, i] > v[j]]
             
             gini_left = 0
             gini_right = 0
 
             for k in range(np.min(labels), np.max(labels)+1):
-                if len(left_docs) != 0:
-                    t1_left = np.true_divide(len(left_docs[left_docs == k]), len(left_docs))
+                if len(left_y) != 0:
+                    t1_left = np.true_divide(len(left_y[left_y == k]), len(left_y))
                     t2_left = np.power(t1_left, 2)
                     gini_left += t2_left
                 
-                if len(right_docs) != 0:
-                    t1_right = np.true_divide(len(right_docs[right_docs == k]), len(right_docs))
+                if len(right_y) != 0:
+                    t1_right = np.true_divide(len(right_y[right_y == k]), len(right_y))
                     t2_right = np.power(t1_right, 2)
-                    gini_left += t2_right
+                    gini_right += t2_right
             gini_left = 1 - gini_left
             gini_right = 1 - gini_right
 
-            t1_gini = (len(left_docs) * gini_right + len(right_docs) * gini_right)
+            t1_gini = (len(left_y) * gini_right + len(right_y) * gini_right)
 
             value = np.true_divide(t1_gini, len(labels))
 
@@ -75,24 +99,53 @@ def convert_labels_to_int_array(labels):
     label_ints = []
     for i in range(len(labels)):
         label_ints.append(i)
-    # print(label_ints)
     return label_ints
+
+def get_top_fifty_features(weights):
+    docs_dict = {}
+    for i in range(len(weights)):
+        docs_dict[weights[i]] = i
+    docs_dict_sorted_keys = sorted(docs_dict)[:50]
+    # print(docs_dict_sorted_keys)
+    top_fifty_features = []
+    for j in docs_dict_sorted_keys:
+        top_fifty_features.append(docs_dict[j])
+    return sorted(top_fifty_features)
 
 newsgroup_train = vectors[:11175]
 newsgroup_test = vectors[11175:]
+labels_train = labels[:11175]
+labels_test = labels[11175:]
+labels_as_ints_train = labels_as_ints[:11175]
+labels_as_ints_test = labels_as_ints[11175:]
+
+print('=====DEFAULT=====')
+three_classifier_tests(newsgroup_train, newsgroup_test, labels_train, labels_test)
+
+print('=====GINI INDEX=====')
+print('getting top fifty features...')
 np_train = np.asarray(newsgroup_train)
-# print(np_vectors.shape)
-int_labels = convert_labels_to_int_array([i for i in range(np_train.shape[0])])
-int_labels = np.asarray(int_labels)
-print(len(int_labels))
-gini = calc_gini_index(np_train, int_labels)
-print(gini)
+gini = calc_gini_index(np_train, np.asarray(labels_as_ints_train))
+top_fifty_features = get_top_fifty_features(gini)
+print('top fifty features: {}'.format(top_fifty_features))
+
+train_top_fifty_only = []
+for i in newsgroup_train:
+    vector_top_fifty = []
+    for j in range(len(i)):
+        if j in top_fifty_features:
+            vector_top_fifty.append(j)
+    train_top_fifty_only.append(vector_top_fifty)
 
 
-# classifier = RandomForestClassifier(max_depth=2)
-# test_classifier(newsgroup_train, newsgroup_test, classifier, vectors, labels)
+test_top_fifty_only = []
+for i in newsgroup_test:
+    vector_top_fifty = []
+    for j in range(len(i)):
+        if j in top_fifty_features:
+            vector_top_fifty.append(j)
+    test_top_fifty_only.append(vector_top_fifty)
 
-# classifier = RandomForestClassifier(n_estimators=1000, max_depth=100, random_state=95)
-# test_classifier(classifier, vectors, labels)
+three_classifier_tests(train_top_fifty_only, test_top_fifty_only, labels_train, labels_test)
 
 
